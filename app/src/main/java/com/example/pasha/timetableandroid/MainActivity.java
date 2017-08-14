6d12f431138a93b22f618650648a88bed23629d8
pasha <pasha_mugako@mail.ru>
2017-08-15 01:49:47 +0300

    come back after holiday

--- app/src/main/java/com/example/pasha/timetableandroid/MainActivity.java
+++ app/src/main/java/com/example/pasha/timetableandroid/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.pasha.timetableandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.*;
 import com.example.pasha.timetableandroid.routes.Route;
 import com.example.pasha.timetableandroid.routes.RouteAdapter;
 
@@ -24,10 +23,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        //initial button on activity
         start = findViewById(R.id.start);
         end = findViewById(R.id.end);
         findRoute = findViewById(R.id.findRoute);
 
+        //initial ListView
         resultView = findViewById(R.id.result);
         RouteAdapter routeAdapter = new RouteAdapter(this, initResult());
         resultView.setAdapter(routeAdapter);
@@ -41,12 +42,23 @@ public class MainActivity extends Activity {
 //        });
     }
 
+    //for menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_items, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_refresh:
+                Toast.makeText(getApplicationContext(),"blablabla", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    //for ListView
     private List<Route> initResult(){
         List<Route> list = new ArrayList<>();
         list.add(new Route("АТОЛИНО - МИНСК АС-ЮгоЗападная","279","05:44", "06:02", "буд"));
@@ -60,4 +72,5 @@ public class MainActivity extends Activity {
         list.add(new Route("АТОЛИНО - МИНСК АС-Дружная","279","06:03", "06:19", "вых"));
         return list;
     }
+
 }
